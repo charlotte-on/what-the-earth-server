ed68b3b2d0c02c60550a926d0b4b1666ad67cbe1
Charlotte Oury <charlotte.ouryb@gmail.com>
2021-01-21 09:06:40 +0100

    result simulator

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipe = new Schema({
-  selectedProducts: [{ String }],
+  products: [{}],
   user: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const Recipe = require("../models/Recipe");
+const User = require("../models/User");
 const router = express.Router();
 
 router.post("/simulator", function (req, res, next) {
-  Recipe.create(req.body)
+  const recipe = { ...req.body };
+  const user = req.session.currentUser;
+  Recipe.create({ products: recipe, user: user })
     .then((recipe) => res.status(200).json(recipe))
     .catch(next);
 });
 
-router.get("/simulator/result", function (req, res, next) {
-  Recipe.findById(req.params.id).then((result) => {
-    res.status(200).json(result);
-  });
+router.get("/simulator/result/:id", function (req, res, next) {
+  Recipe.findById(req.params.id)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(next);
 });
 
 module.exports = router;
