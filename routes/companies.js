2496251787086cfcfb49a78f7963efbe74a37162
Victorien Provenzano <victorien.provenzano@icloud.com>
2021-01-21 11:33:01 +0100

    fixed auth

--- routes/companies.js
+++ routes/companies.js
@@ -7,7 +7,7 @@ const requireAuth = require("../middlewares/requireAuth");
 
 const salt = 10;
 
-router.get("/", requireAuth, (req, res, next) => {
+router.get("/", (req, res, next) => {
   Companies.find()
     .then((companiesDocument) => {
       res.status(200).json(companiesDocument);
@@ -17,7 +17,7 @@ router.get("/", requireAuth, (req, res, next) => {
     });
 });
 
-router.get("/:id", requireAuth, (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   Companies.findById(req.params.id)
     .select("-password")
     .then((companiesDocument) => {
@@ -39,17 +39,22 @@ router.get("/edit/:id", requireAuth, (req, res, next) => {
     });
 });
 
-router.patch("/edit/:id", requireAuth, (req, res, next) => {
-  Companies.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((companiesDocument) => {
-      res.status(200).json(companiesDocument);
-    })
-    .catch((error) => {
-      next(error);
-    });
-});
+router.patch(
+  "/edit/:id",
+  upload.single("bannerImg"),
+  requireAuth,
+  (req, res, next) => {
+    Companies.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then((companiesDocument) => {
+        res.status(200).json(companiesDocument);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
 
-router.post("/signin", requireAuth, (req, res, next) => {
+router.post("/signin", (req, res, next) => {
   const { email, password } = req.body;
   Companies.findOne({ email })
     .then((userDocument) => {
@@ -72,63 +77,58 @@ router.post("/signin", requireAuth, (req, res, next) => {
     .catch(next);
 });
 
-router.post(
-  "/signup",
-  requireAuth,
-  upload.single("bannerImg"),
-  (req, res, next) => {
-    const {
-      email,
-      password,
-      companyName,
-      firstName,
-      lastName,
-      phoneNumber,
-      schedule,
-      field,
-      description,
-      location,
-      formattedAddress,
-      bannerImg,
-    } = req.body;
+router.post("/signup", upload.single("bannerImg"), (req, res, next) => {
+  const {
+    email,
+    password,
+    companyName,
+    firstName,
+    lastName,
+    phoneNumber,
+    schedule,
+    field,
+    description,
+    location,
+    formattedAddress,
+    bannerImg,
+  } = req.body;
 
-    if (req.file) {
-      req.body.bannerImg = req.file.path;
-    }
-
-    Companies.findOne({ email })
-      .then((userDocument) => {
-        if (userDocument) {
-          return res.status(400).json({ message: "Email already taken" });
-        }
-
-        const hashedPassword = bcrypt.hashSync(password, salt);
-        const newCompany = {
-          email,
-          password: hashedPassword,
-          companyName,
-          firstName,
-          lastName,
-          phoneNumber,
-          schedule,
-          field,
-          description,
-          location,
-          formattedAddress,
-          bannerImg: req.body.bannerImg,
-        };
-
-        Companies.create(newCompany)
-          .then((newUserDocument) => {
-            /* Login on signup */
-            req.session.producer = true;
-            req.session.currentUser = newUserDocument._id;
-            res.redirect("/api/auth/isLoggedIn");
-          })
-          .catch(next);
-      })
-      .catch(next);
+  if (req.file) {
+    req.body.bannerImg = req.file.path;
   }
-);
+
+  Companies.findOne({ email })
+    .then((userDocument) => {
+      if (userDocument) {
+        return res.status(400).json({ message: "Email already taken" });
+      }
+
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      const newCompany = {
+        email,
+        password: hashedPassword,
+        companyName,
+        firstName,
+        lastName,
+        phoneNumber,
+        schedule,
+        field,
+        description,
+        location,
+        formattedAddress,
+        bannerImg: req.body.bannerImg,
+      };
+
+      Companies.create(newCompany)
+        .then((newUserDocument) => {
+          /* Login on signup */
+          req.session.producer = true;
+          req.session.currentUser = newUserDocument._id;
+          res.redirect("/api/auth/isLoggedIn");
+        })
+        .catch(next);
+    })
+    .catch(next);
+});
 
 module.exports = router;
