2f9cef99945c84ea9737174163f76195df2441d5
Victorien Provenzano <victorien.provenzano@icloud.com>
2021-01-21 18:28:04 +0100

    merge

--- models/User.js
+++ models/User.js
@@ -6,23 +6,14 @@ const userSchema = new Schema(
     image: { type: String, default: "/media/avatar.png" },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    // role: {
-    //   type: String,
-    //   required: true,
-    //   enum: ["admin", "user"],
-    //   default: "user",
-    // },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    // profilePic: String,
-    // favorites: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "Producer",
-    // },
-    // shopFrom: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "Shop",
-    // },
+    favoriteProducers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Company",
+      },
+    ],
   },
   { timestamps: true }
 );
