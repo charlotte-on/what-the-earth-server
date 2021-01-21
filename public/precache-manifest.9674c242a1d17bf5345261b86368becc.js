59d4eec765ee2ece9a05bb105b9c5a86c5d89bf9
Calypso Asline <aslinecalypso@gmail.com>
2021-01-21 11:17:45 +0100

    what-the-earth commit

---
+++ public/precache-manifest.9674c242a1d17bf5345261b86368becc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff5fe2d97ee577f3303ced535469860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7efc0e892500ec876e84",
+    "url": "/static/css/2.bdca64d0.chunk.css"
+  },
+  {
+    "revision": "7d9e286be506a2e5eb2c",
+    "url": "/static/css/main.264fb922.chunk.css"
+  },
+  {
+    "revision": "7efc0e892500ec876e84",
+    "url": "/static/js/2.148d1414.chunk.js"
+  },
+  {
+    "revision": "73cd9c7b5a2b4252dbdce14c773ec693",
+    "url": "/static/js/2.148d1414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9e286be506a2e5eb2c",
+    "url": "/static/js/main.2d75b42e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
